4ab700538d93329725ba3add94d14e1f4080274e
zzq <i@zhangzqs.cn>
2025-02-02 10:54:04 +0800

    优化debug组件

--- debug/component.go
+++ debug/component.go
@@ -4,12 +4,14 @@ import (
 	"github.com/go-compcont/compcont-core"
 	compcontzap "github.com/go-compcont/compcont-std/compcont-zap"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 const TypeID compcont.ComponentTypeID = "std.debug"
 
 type Config struct {
 	Logger  *compcont.TypedComponentConfig[any, *zap.Logger] `ccf:"logger"`
+	Level   string                                           `ccf:"level"`
 	Message string                                           `ccf:"message"`
 }
 
@@ -28,7 +30,20 @@ var factory compcont.IComponentFactory = &compcont.TypedSimpleComponentFactory[C
 			logger = compcontzap.GetDefault()
 		}
 
-		logger.Debug(
+		var level zapcore.Level
+		if cfg.Level != "" {
+			lvl, err1 := zap.ParseAtomicLevel(cfg.Level)
+			if err1 != nil {
+				logger.Error("parse zap level error", zap.Error(err1))
+				return
+			}
+			level = lvl.Level()
+		} else {
+			level = zapcore.DebugLevel
+		}
+
+		logger.Log(
+			level,
 			"compcont debug info",
 			zap.Stringer("name", ctx.Config.Name),
 			zap.Stringer("type", ctx.Config.Type),
